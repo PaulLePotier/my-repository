b713059500d92324e7669cf4c94a8ef29315434f
Petso <petso@MacBook-Pro-de-paul-Lescure.local>
2023-11-18 11:21:35 +0100

    modifcation index pour test live

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect("mongodb://localhost:27017/Vinted");
 
 const userRoutes = require("./routes/user");
 app.use(userRoutes);
